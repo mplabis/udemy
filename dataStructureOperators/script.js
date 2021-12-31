2c429e7abc6eca2a33e211cd050a26f92a21d75f
mplabis <mcphilliplabis@gmail.com>
2021-12-31 15:54:40 +0800

    nullish value ??

--- dataStructureOperators/script.js
+++ dataStructureOperators/script.js
@@ -1,16 +1,16 @@
-'use strict';
+"use strict";
 
 // Data needed for a later exercise
 const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
 // Data needed for first part of the section
 const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
 
   openingHours: {
     thu: {
@@ -47,7 +47,7 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
-restaurant.orderPizza('mushrooms', 'ahus', 'sibuyas', 'luya');
+restaurant.orderPizza("mushrooms", "ahus", "sibuyas", "luya");
 
 // const add = function (...numbers) {
 //   let sum = 0;
@@ -143,3 +143,25 @@ restaurant.orderPizza('mushrooms', 'ahus', 'sibuyas', 'luya');
 // const string = 'makko';
 // const letters = [...string, 'P.'];
 // console.log(`${letters}`);
+
+// console.log(3 || "makko");
+// console.log(true || 0);
+// console.log("" || "makko");
+// console.log(undefined || null);
+// console.log(undefined || 0 || "" || "hello" || 23 || null);
+// console.log(typeof null);
+
+// const guests1 = restaurant.numGuest ? restaurant.numGuest : 10;
+// console.log(guests1);
+restaurant.numGuests = 0;
+const guests2 = restaurant.numGuests ?? 10;
+console.log(guests2);
+
+// console.log("---AND---");
+// console.log("makko" && 23 && 24 && "wawawa");
+// if (restaurant.orderPizza) {
+//   restaurant.orderPizza("mushrooms", "spinach");
+// }
+// restaurant.orderPizza && restaurant.orderPizza("luya", "sibuyas", "ahus");
+
+// console.log(typeof restaurant.orderPizza);
